d8a55be2b54622c5f25339ed9aecc5e2f6af631a
Maya Kreidieh <mayakreidieh@gmail.com>
2013-10-22 21:07:26 -0400

    Add comments to statemachine.js

--- statemachine.js
+++ statemachine.js
@@ -1,52 +1,64 @@
-// Your task is to fill in the rest of this file with your state machine, and then save
-// the file to statemachine.js.
+/** 
+  * @desc 	Initalizes a State Machine object and binds it to an 
+  * 		element in the DOM. Binds event listeners to the DOM object
+  * @param 	description - description of the state machine object
+  *			elementToAttach - the DOM elment to attach the state machine to
+*/  
 function StateMachine(description, elementToAttach) {
 
+
+	// Set the state machine's fields given the inital parameters
+	// currentState = the current state of the statemachine
+	// stateTable = the description
+	// domElement = the DOM object to attach the state machine to
 	this.stateTable = description.states;
 	this.currentState = description.states[0].name;
 	this.domElement = elementToAttach;
 
+	// Attach the statemachine to its DOM element 
 	$(elementToAttach).data({
 		'stateMachine' : this
 	});
 
-	var domEvents = ['mousedown', 'mouseup', 'click', 'mousemove', 'mouseover', 'mouseout', 'keypress', 'customSetInterval', 'focus', 'focusout', 'stopTetrisPiece', 'appendTetrisPiece','appendNewTetrisPiece'];
+	// Arrays of the rawInputEvents and the corresponsding stateMachineEvents
+	// where rawInputEvent[i] will be tranlsated into stateMachineEvent[i] which
+	// can be interpreted by the state machine.
+	var rawInputEvents = ['mousedown', 'mouseup', 'click', 'mousemove', 'mouseover', 'mouseout', 'keypress', 'customSetInterval', 'focus', 'focusout', 'stopTetrisPiece', 'appendTetrisPiece','appendNewTetrisPiece'];
 	var stateMachineEvents = ['mouseDown','mouseUp','click','mouseMove','mouseIn','mouseOut','keyPress', 'timerTick30Ms','focus','focusout', 'stopTetrisPiece', 'appendTetrisPiece','appendNewTetrisPiece'];
-	// var domEvents = ['mousedown'];
-	// var stateMachineEvents = ['mouseDown'];
 
 	var that = this;
 
-	console.log((elementToAttach));
-(elementToAttach).addEventListener("customSetInterval", function(e) {
-	console.info("Event is: ", e);
-	console.info("Custom data is: ", e.detail);
-})
-
-
-	for (var i=0;i<domEvents.length;i++){
+	// Bind each raw input event to the updating the state of the machine given
+	// its corresponding state machine interpretable events.
+	for (var i=0;i<rawInputEvents.length;i++){
 
 		(function(){ var input_type = stateMachineEvents[i];
 
-		$(elementToAttach).bind(domEvents[i], function(event){
+		$(elementToAttach).bind(rawInputEvents[i], function(event){
 			that.updateState(event, input_type);
 		}); }());
-
 	}
-	// input_type = stateMachineEvents[ domEvents.length ];
-
 }
 
+/** 
+  * @desc 	When an event occurs, updates the state of the state machine
+  * 		by completeing the proper action and updating the state
+  * @param 	input_event - description of the state machine object
+  *			input_type  - the DOM elment to attach the state machine to
+*/  
 StateMachine.prototype.updateState = function( input_event, input_type ){
 
 	var transitions = [];
 
+	// Find the proper transitions for the current state
 	for (var i=0;i<this.stateTable.length;i++){
 		if (this.stateTable[i].name == this.currentState){
 			transitions = this.stateTable[i].transitions;
 		}
 	}
 
+	// Given the input_event, call the proper action and update
+	// the state of the state machine
 	for (var i=0;i<transitions.length;i++){
 		if (transitions[i].input == input_type){
 			transitions[i].action( input_event, this.domElement );
